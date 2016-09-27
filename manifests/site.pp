4e2ccc7f0025b291f8013b688b6176838d860705
nixgeekgirl <nixgeekgirl@gmail.com>
2016-09-27 15:14:14 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -51,9 +51,9 @@ node default {
 #  mode => '0644',
 #  content => "I love burritos\n",
 #  }
-  exec {"cowsay 'Welcome to ${::fqdn}!'>/etc/motd":
-    path => '/usr/bin:/usr/local/bin',
-     creates => '/etc/motd',
-  }
+#exec {"cowsay 'Welcome to ${::fqdn}!'>/etc/motd":
+ #   path => '/usr/bin:/usr/local/bin',
+  #   creates => '/etc/motd',
+  #}
 
 include users
