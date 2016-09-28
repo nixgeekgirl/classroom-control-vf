70588132c3274b12bc7fd9c4cb7132e6d9845954
nixgeekgirl <nixgeekgirl@gmail.com>
2016-09-28 00:23:55 -0400

    Update init.pp

--- users/manifests/init.pp
+++ users/manifests/init.pp
@@ -1,6 +1,6 @@
 class users {
-user { 'fundamentals':
-ensure => present,
+user {'fundamentals':
+  ensure => present,
 # password => 'puppet8#labs', # Windows requires a plain text password
 # groups => ['Users'], # Display in Windows Control Panel
 }
