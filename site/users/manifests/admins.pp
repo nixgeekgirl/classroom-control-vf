2efedca65d4bb1a4135e33e5d5f40343d3ac65f6
nixgeekgirl <nixgeekgirl@gmail.com>
2016-09-28 16:41:09 -0400

    Update admins.pp

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -8,11 +8,9 @@ group => 'staff',
 }
 users::managed_user { 'jc':
 group => 'admin',
-gid => '10',
 }
 users::managed_user { 'matthew': 
 group => 'staff', 
-#  gid             => ['wheel', 'memcached'], 
 } 
 group { 'staff':
 ensure => present,
