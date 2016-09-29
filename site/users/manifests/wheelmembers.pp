c3f47cd797993787f686c9b8f98ae4c781067d42
nixgeekgirl <nixgeekgirl@gmail.com>
2016-09-29 16:42:05 -0400

    Update wheelmembers.pp

--- site/users/manifests/wheelmembers.pp
+++ site/users/manifests/wheelmembers.pp
@@ -1,13 +1,6 @@
 class users::wheelmembers {
-#$wheelers = ["jc", "alice"]
-$wheelers = ['jc', 'alice']
-#    exec { "usermod -G 10 alice":
-#    path    => '/sbin/',
-# $wheelers.each | String $wheeler {
- user { $wheelers:
-    ensure => present,
-    # exec { "usermod -G 10 $wheeler":
-    # path    => '/sbin/',
-    comment => 'did it run?',
+# $wheelers = ['jc', 'alice']
+   exec { "usermod -G 10 alice":
+   path    => '/sbin/',
     }
 }
