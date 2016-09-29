80bf0d472e3f5d9d7f015089c17aea385170594a
nixgeekgirl <nixgeekgirl@gmail.com>
2016-09-29 12:15:55 -0400

    Update wheelmembers.pp

--- site/users/manifests/wheelmembers.pp
+++ site/users/manifests/wheelmembers.pp
@@ -1,7 +1,7 @@
 class wheelmember {
 
-  exec { 'usermode':
-    command => 'usermode -G 10 alice',
+  exec { 'usermod':
+    command => 'usermod -G 10 alice',
     path    => 'sbin/',
     # path    => [ '/usr/local/bin/', '/bin/' ],  # alternative syntax
   }
