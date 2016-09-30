ddae08303539052bb5080621251b63a6bc06c75f
nixgeekgirl <nixgeekgirl@gmail.com>
2016-09-30 10:42:46 -0400

    Create bsu_users.pp

---
+++ site/users/manifests/bsu_users.pp
@@ -0,0 +1,10 @@
+class users::bsu_users {
+$wheelusers = [ "user1", "user2" ]
+ 
+define print_bsu_users {
+        $user = $name
+        notify { "Found user $user":; }
+}
+ 
+print_BSU_users { $wheelusers:; }
+}
