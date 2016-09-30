261a4f7c509cb2c282f7095368f58cc7d539b7bd
nixgeekgirl <nixgeekgirl@gmail.com>
2016-09-30 14:57:03 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -47,10 +47,10 @@ if $::virtual != 'physical' {
 $vmname = capitalize($::virtual)
 notify { "Hi from BSU on Thursday.  This is a ${vmname} virtual machine.": }
 
-class { 'nginx':
+# class { 'nginx':
 #root => '/var/www/html',
-}
-}
+# }
+# }
 
 #$message = hiera('message')
 #notify { $message: }
@@ -91,7 +91,7 @@ class helloworld {
     notify { 'hello, world!': }
  }
 
-include hellowworld
+include helloworld
 
 #}
 
