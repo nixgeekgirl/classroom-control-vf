b57d791bf0ec83436ee8e22a9a8fef8878b4ff4b
nixgeekgirl <nixgeekgirl@gmail.com>
2016-09-30 12:34:09 -0400

    Update someusers.pp

--- site/users/manifests/someusers.pp
+++ site/users/manifests/someusers.pp
@@ -8,7 +8,14 @@ class users::someusers {
  
 # print_users { $users:; }
 # filter out all the integers less than zero
-$array_in = [-4,3,-8,-2,1,4,-2,1,5,-1,-7,9,-3,2,6,-8,5,3,5,-6,8,9,7,-5,9,3,-3]
-$array_out = split(inline_template('<%= array_in.delete_if {|x| x < 0 }.join(",") %>'), ',')
-notice($array_out)
+#$array_in = [-4,3,-8,-2,1,4,-2,1,5,-1,-7,9,-3,2,6,-8,5,3,5,-6,8,9,7,-5,9,3,-3]
+#$array_out = split(inline_template('<%= array_in.delete_if {|x| x < 0 }.join(",") %>'), ',')
+#notice($array_out)
+$files = [
+  '/etc/puppet/puppet.conf',
+  '/etc/puppet/auth.conf',
+  '/etc/puppet/routes.yaml',
+]
+
+puppet::file { $files: }
 }
