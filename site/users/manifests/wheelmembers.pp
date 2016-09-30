e261d106e36994488f5e21074668f6d11674df63
nixgeekgirl <nixgeekgirl@gmail.com>
2016-09-30 10:26:41 -0400

    Update wheelmembers.pp

--- site/users/manifests/wheelmembers.pp
+++ site/users/manifests/wheelmembers.pp
@@ -1,8 +1,8 @@
 class users::wheelmembers {
 $wheelers = ['jc', 'alice']
-   # exec { "usermod -G 10 alice":
-   # path    => '/sbin/',
-   # }
+   exec { "usermod -G 10 alice":
+   path    => '/sbin/',
+   }
     # wheelers.foreach { notify "BSU wheeler $x" }
-    wheelers.foreach { |$x| notify { "Hi from BSU on Friday.  This is a $x .": }}
+    # wheelers.foreach { |$x| notify { "Hi from BSU on Friday.  This is a $x .": }}
 }
