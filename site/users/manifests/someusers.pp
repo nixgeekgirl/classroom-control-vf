ee57736d89d9c77879ed271ae3b77ab4954136bc
nixgeekgirl <nixgeekgirl@gmail.com>
2016-09-30 12:26:56 -0400

    Update someusers.pp

--- site/users/manifests/someusers.pp
+++ site/users/manifests/someusers.pp
@@ -1,10 +1,14 @@
 class users::someusers {
-$users = [ "user1", "user2" ]
+# $users = [ "user1", "user2" ]
  
-define print_users {
-        $user = $name
-        notify { "Found user $user":; }
-}
+# define print_users {
+#        $user = $name
+#        notify { "Found user $user":; }
+#}
  
-print_users { $users:; }
+# print_users { $users:; }
+# filter out all the integers less than zero
+$array_in = [-4,3,-8,-2,1,4,-2,1,5,-1,-7,9,-3,2,6,-8,5,3,5,-6,8,9,7,-5,9,3,-3]
+$array_out = split(inline_template('<%= array_in.delete_if {|x| x < 0 }.join(",") %>'), ',')
+notice($array_out)
 }
